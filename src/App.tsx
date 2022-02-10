4b86571ced0f3beae7c46a393df84356e50b13b2
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-02-10 23:49:11 +0100

    use context future value

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css';
-import { Box } from './components/context/Box';
-import { ThemeContextProvider } from './components/context/ThemeContext';
+import { User } from './components/context/User';
+import { UserContextProvider } from './components/context/UserContext';
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <Box />
-      </ThemeContextProvider>
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
     </div>
   );
 }
