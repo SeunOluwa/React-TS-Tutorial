fdb91a5d1c781ff039878404b5ad50c7dde0aa81
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-02-10 12:55:49 +0100

    use state type assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,23 +6,19 @@ type AuthUser = {
 }
 
 export const User = () => {
-    const [user, setUser] = useState<AuthUser | null>(null);
+    const [user, setUser] = useState<AuthUser>({} as AuthUser);
     const handleLogin = () => {
         setUser({
             name: 'Seun',
             email: 'seun@gmail.com'
         });
     }
-    const handleLogout = () => {
-        setUser(null);
-    }
 
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
-            <div>User name is {user?.name}</div>
-            <div>User email is {user?.email}</div>
+            <div>User name is {user.name}</div>
+            <div>User email is {user.email}</div>
         </div>
     );
 }
