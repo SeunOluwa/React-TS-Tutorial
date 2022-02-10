4705da6392739413e086ebac19b0ed027e562fdc
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-02-10 21:29:28 +0100

    use context hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
+import { Box } from './components/context/Box';
+import { ThemeContextProvider } from './components/context/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-      
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
---
+++ src/components/context/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+    primary: {
+        main: '#3f51bf',
+        text: '#fff'
+    },
+    secondary: {
+        main: '#f50057',
+        text: '#fff'
+    }
+}
