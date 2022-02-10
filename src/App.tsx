fdaea70c55757fde15013bca5d8115d12a3e0324
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-02-10 09:15:59 +0100

    basic props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Seun',
+    last: 'Taiwo'
+  }
+
+  const nameList = [
+    {
+      first: 'Seun',
+      last: 'Taiwo'
+    },
+    {
+      first: 'Seyi',
+      last: 'Taiwo'
+    },
+    {
+      first: 'Hannah',
+      last: 'Taiwo'
+    }
+  ]
+
   return (
     <div className="App">
-      <Greet name="Seun" />
+      <Greet name="Seun" messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
