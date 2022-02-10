94ee558d16de01d6dea039b7ecc23d370a72251e
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-02-10 09:56:37 +0100

    advanced props

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
 import './App.css';
 import { Greet } from './components/Greet';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
+import { Status } from './components/Status';
 
 function App() {
-  const personName = {
-    first: 'Seun',
-    last: 'Taiwo'
-  }
-
-  const nameList = [
-    {
-      first: 'Seun',
-      last: 'Taiwo'
-    },
-    {
-      first: 'Seyi',
-      last: 'Taiwo'
-    },
-    {
-      first: 'Hannah',
-      last: 'Taiwo'
-    }
-  ]
-
   return (
     <div className="App">
-      <Greet name="Seun" messageCount={20} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
+      <Status status='loading' />
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Seun Taiwo!</Heading>
+      </Oscar>
+      <Greet name='Seun' isLoggedIn={true} />
     </div>
   );
 }
---
+++ src/components/Validations.tsx
@@ -0,0 +1,5 @@
+export const Validations = () => {
+    return (
+        <div></div>
+    );
+}
