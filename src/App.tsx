0c5b5fdeb9f47fd39b4110cc63bfb9372213c2fd
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-02-10 10:56:09 +0100

    prop types and tips

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css';
-import { Container } from './components/Container';
 
 function App() {
   return (
     <div className="App">
-      <Container styles={{ border: '1px solid black', padding: '1rem' }} />
+      
     </div>
   );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,12 +5,12 @@ type InputProps = {
     handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const Input = (props: InputProps) => {
+export const Input = ({ value, handleChange }: InputProps) => {
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         console.log(event);
     }
 
     return (
-        <input type="text" value={props.value} onChange={handleInputChange} />
+        <input type="text" value={value} onChange={handleChange} />
     );
 }
