f5a6d52fdcb2901fc591bfa087455bdb29753357
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-02-11 12:29:30 +0100

    use ref hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css';
-import { User } from './components/context/User';
-import { UserContextProvider } from './components/context/UserContext';
+import { DomRef } from './components/ref/DomRef';
+import { MutableRef } from './components/ref/MutableRef';
 
 function App() {
   return (
     <div className="App">
-      <UserContextProvider>
-        <User />
-      </UserContextProvider>
+      <DomRef />
+      <MutableRef />  
     </div>
   );
 }
