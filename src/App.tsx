85a0977675ad7dd21be32bdc4d8054fc21931826
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-02-13 13:07:13 +0100

    restricting props

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
 import './App.css';
-import { List } from './components/generics/List';
+import { RandomNumber } from './components/restriction/RandomNumber';
 
 function App() {
   return (
     <div className="App">
-      {/* <List 
-        items={['Seun', 'Seyi', 'Hannah']}
-        onClick={(item) => console.log(item)}
-      />
-      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
-      <List 
-        items={[
-          {
-            id: 1,
-            first: 'Seun',
-            last: 'Taiwo'
-          },
-          {
-            id: 2,
-            first: 'Seyi',
-            last: 'Taiwo'
-          },
-          {
-            id: 3,
-            first: 'Hannah',
-            last: 'Taiwo'
-          }
-        ]}
-        onClick={(item) => console.log(item)}
-      />
+      <RandomNumber value={10} isPositive />
     </div>
   );
 }
