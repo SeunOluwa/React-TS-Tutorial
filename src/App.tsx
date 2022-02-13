5c660c71da0a39cbdae51577e3773c1c6b1dabb8
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-02-13 22:48:10 +0100

    polymorphic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css';
+import { Text } from './components/polymorphic/Text';
 
 function App() {
   return (
     <div className="App">
-      
+      <Text as='h1' size='lg'>Heading</Text>
+      <Text as='p' size='md'>Paragraph</Text>
+      <Text as='label' htmlFor='someId' size='sm' color='secondary'>
+        Label
+      </Text>
     </div>
   );
 }
